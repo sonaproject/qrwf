9b8744b682d4b341d077a35e5642a032882d455f
goodhyoju <good_hyoju@hanmail.net>
2023-05-30 15:47:24 +0900

    add load generator and error generator

--- src/main/java/com/goblin/qrwf/service/MonitoringService.java
+++ src/main/java/com/goblin/qrwf/service/MonitoringService.java
@@ -220,7 +220,7 @@ public class MonitoringService {
      */
     @Async
     public void MEMORYLoadGenerator(long duration) {
-        int elementSize = 1024;
+        int elementSize = 512;
         int numElements = 100000;
         long startTime = System.currentTimeMillis();
 
