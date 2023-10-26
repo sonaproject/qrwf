01e5d478d60e1ab05fa4cfc5a3576c9f59d5217e
Daniel Park <dan.mcpark84@gmail.com>
2023-10-26 23:13:51 +0000

    updated.

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ echo ">>>>> Start $NAME"
 
 #nohup java -jar /home/ubuntu/qrwf/build/libs/qrwf-1.0.jar -XX:+PrintCompilation -XX:CompileCommand=exclude,*.CPULoadGenerator -Xint -XX:+UseG1GC -Dlog4j2.formatMsgNoLookups=true -Dinflux.url=10.0.0.243:8086 -Dinflux.username=edgecloud -Dinflux.password=edge123! -Dinflux.token=wsRkPDMrs0Vnu2RFU-IkFEBPxtgwqPGVD7clS3iMr9qOXrQynw0PTcMbQh7sOmLcLcJvf4-wcl1PtvZrYDSGiw== -Dinflux.org=edgecloud > output.log 2>&1
 
-nohup java -XX:+PrintCompilation -XX:CompileCommand=compileonly,*.indexOf  -XX:+UseG1GC -Dlog4j2.formatMsgNoLookups=true -Dinflux.url=10.0.0.243:8086 -Dinflux.username=edgecloud -Dinflux.password=edge123! -Dinflux.token=wsRkPDMrs0Vnu2RFU-IkFEBPxtgwqPGVD7clS3iMr9qOXrQynw0PTcMbQh7sOmLcLcJvf4-wcl1PtvZrYDSGiw== -Dinflux.org=edgecloud -jar /home/ubuntu/qrwf/build/libs/qrwf-1.0.jar > output.log 
+nohup java -XX:+UseG1GC -Dlog4j2.formatMsgNoLookups=true -Dinflux.url=10.0.0.243:8086 -Dinflux.username=edgecloud -Dinflux.password=edge123! -Dinflux.token=wsRkPDMrs0Vnu2RFU-IkFEBPxtgwqPGVD7clS3iMr9qOXrQynw0PTcMbQh7sOmLcLcJvf4-wcl1PtvZrYDSGiw== -Dinflux.org=edgecloud -jar /home/ubuntu/qrwf/build/libs/qrwf-1.0.jar > output.log 2>&1
 
 #nohup java -Xint -XX:+UseG1GC -Dlog4j2.formatMsgNoLookups=true -Dinflux.url=10.0.0.243:8086 -Dinflux.username=edgecloud -Dinflux.password=edge123! -Dinflux.token=wsRkPDMrs0Vnu2RFU-IkFEBPxtgwqPGVD7clS3iMr9qOXrQynw0PTcMbQh7sOmLcLcJvf4-wcl1PtvZrYDSGiw== -Dinflux.org=edgecloud -jar /home/ubuntu/qrwf/build/libs/qrwf-1.0.jar > output.log 2>&1
 
